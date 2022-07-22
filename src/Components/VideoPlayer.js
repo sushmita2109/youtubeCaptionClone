af70b78b21286f8dbee8d08e36559ad2142cba3f
sushmitakumari <sushmitakumari363@gmail.com>
2022-07-22 22:01:52 +0530

    Video-player

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css';
+import ReactPlayer from 'react-player';
+
 function VideoPlayer() {
   return (
     <div className='video-player'>
       <div className='video'>
-          <h1>Video</h1>
+          <ReactPlayer width='450px' height='280px' controls url='https://youtu.be/7sDY4m8KNLc'></ReactPlayer>
       </div>
       <div className='video-pause'>
           <small>Enter subtitle faster with the keyboard shortcut</small>
